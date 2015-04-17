a340826b923fc50a441d711e03b8f37c8d3170de
Justin Hyunmok Jeong <jj@j2.io>
2015-04-17 17:11:00 -0400

    Update lolfrontpagebot.py

--- lolfrontpagebot.py
+++ lolfrontpagebot.py
@@ -9,7 +9,7 @@ access_token_secret = 'ACCESS_TOKEN_SECRET_HERE'
 consumer_key = 'CONSUMER_KEY_HERE'
 consumer_secret = 'CONSUMER_KEY_SECRET_HERE'
 
-def strip_title(title):   #Make sure that the tweet fits within 140 characters
+def strip_title(title):
   if len(title) < 114:
     return title
   else:
@@ -19,7 +19,7 @@ def tweet_creator(subreddit):
     post_content = []
     post_ids = []
     print "[bot] Getting posts from Reddit"
-    reddit=requests.get("http://www.reddit.com/r/{}/.json?limit=25".format(subreddit),   #25 hottest posts for front page
+    reddit=requests.get("http://www.reddit.com/r/{}/.json?limit=25".format(subreddit),
       headers={'User-Agent': 'my reddit bot 123',})
     submissions=reddit.json()["data"]["children"]
     for submission in submissions:
@@ -27,11 +27,11 @@ def tweet_creator(subreddit):
       post_ids.append(submission["data"]["id"])
     return post_ids, post_content
 
-def duplicate_check(id):   #Checks duplicate by seeing if file in in folder index with filename id
-  return os.path.isfile('index/'+id)
+def duplicate_check(id):
+  return os.path.isfile('Scripts/lolfrontpagebot/index/'+id)
 
-def add_id_to_file(id):    #Makes empty file in folder index with filename id
-  open('index/'+id, 'a').close
+def add_id_to_file(id):
+  open('Scripts/lolfrontpagebot/index/'+id, 'a').close
 
 def main():
   post_ids, post_content = tweet_creator("leagueoflegends")
@@ -45,7 +45,6 @@ def tweeter(post_ids, post_content):
     found = duplicate_check(post_id)
     if found == 0:
       print "[bot] Posting this link on twitter: " + post_id
-      print post
       api.update_status(post)
       add_id_to_file(post_id)
       time.sleep(2)
